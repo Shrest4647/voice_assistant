0a634d3ca6cbd7bcbb4b03eeaf20a57620d4df45
Sunil Shrestha <shrest464760+github@gmail.com>
2023-07-08 22:43:09 +0545

    Created a simple voice assistant with langchain

---
+++ run.sh
@@ -0,0 +1,6 @@
+# python -m venv env
+
+source env/Scripts/activate
+
+# pip install langchain openai SpeechRecognition pyttsx3 PyAudio pysoundfile torchvision whisper openai-whisper python-dotenv
+python voiceassistant.py
